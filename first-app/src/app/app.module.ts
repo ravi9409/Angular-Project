6c0a465124d7c22c518f8e0f95f06a7454be5a35
ravi ranjan <raviranjan9409@gmail.com>
2023-04-04 20:19:11 +0530

    added

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {WarningAlertComponent} from "./WarningAlert/warningalert.component";
-import { SucessAlertsComponent } from './sucess-alerts/sucess-alerts.component';
+import { ServerComponent } from './server/server.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SucessAlertsComponent
+    ServerComponent
   ],
   imports: [
     BrowserModule,
---
+++ first-app/src/app/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  allowNewServer:boolean=true;
+  serverCreationStatus='No server was created!';
+  serverName=''
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=false
+    },3000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+  this.serverCreationStatus='Server was created!'
+  }
+
+  onUpdateServerName(event:any){
+  this.serverName=event.target.value;
+  }
+}
